f085b511a5271f9b7c0eebf7cec97c92ad549cf2
Md. Shazzadur Rahman Piash <147403613+3427193711@users.noreply.github.com>
2026-05-11 23:52:00 +0600

    Added hero section

--- js/script.js
+++ js/script.js
@@ -13,3 +13,60 @@ if (menuBtn && navLinks) {
         }
     });
 }
+
+
+
+// Typing Effect
+
+const texts = [
+    "Frontend And Backend Developers",
+    "Creative Designers",
+    "Future Software Engineers",
+    "Team Collaboration Experts"
+];
+
+let textIndex = 0;
+let charIndex = 0;
+
+const typingText = document.querySelector(".typing-text");
+
+function typeEffect() {
+
+    if (charIndex < texts[textIndex].length) {
+
+        typingText.innerHTML += texts[textIndex].charAt(charIndex);
+
+        charIndex++;
+
+        setTimeout(typeEffect, 100);
+
+    } else {
+
+        setTimeout(eraseEffect, 1500);
+    }
+}
+
+function eraseEffect() {
+
+    if (charIndex > 0) {
+
+        typingText.innerHTML =
+            texts[textIndex].substring(0, charIndex - 1);
+
+        charIndex--;
+
+        setTimeout(eraseEffect, 50);
+
+    } else {
+
+        textIndex++;
+
+        if (textIndex >= texts.length) {
+            textIndex = 0;
+        }
+
+        setTimeout(typeEffect, 300);
+    }
+}
+
+typeEffect();
